665c309530fe6eca1d587f553d26018c061354d2
edsonbrunoa <bruno.alves.ba442@gmail.com>
2024-11-27 12:22:30 -0300

    +1 desafio concluido - POO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,20 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
 
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero:"91085713", modelo:"14", imei:"1234", memoria:64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\nSmartphone Nokia:");
+Smartphone nokia = new Nokia(numero:"74001936", modelo:"13c", imei:"4321", memoria:128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
